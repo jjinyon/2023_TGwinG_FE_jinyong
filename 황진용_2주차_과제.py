d2b336b4bda7893b680c1f63ab4ed04646576f83
jjinyon <123253382+jjinyon@users.noreply.github.com>
2023-03-27 23:15:25 +0900

    Update 황진용_2주차_과제.py

--- 황진용_2주차_과제.py
+++ 황진용_2주차_과제.py
@@ -36,7 +36,6 @@ def reverseStr(string):
 
 #8
 def introduce():
-    
     return "제 이름은 {이름} 입니다. 취미는 {취미} 입니다. 저는 {재학중인학교}를 다니고 있습니다. 제 생일은 {yyddmm} 입니다.".format(이름 = input("이름을 입력하세요 : "),취미 = input("취미를 입력하세요 : "),재학중인학교 = input("재학 중인 학교를 입력하세요 : "),yyddmm = input("생일을 입력하세요 화: "))
 print(introduce())
 
